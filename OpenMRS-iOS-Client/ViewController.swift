7cce6ab26925df9ed00fdaa3331831a196f99839
suthagar23 <suthagar23@gmail.com>
2019-05-17 10:52:56 +0530

    Initiated Project

---
+++ OpenMRS-iOS-Client/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenMRS-iOS-Client
+//
+//  Created by Kailayapathy Suthagar on 5/17/19.
+//  Copyright © 2019 OpenMRS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
