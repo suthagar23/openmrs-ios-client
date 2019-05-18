6c58314b1f386faed4856d4845f2687560fac75e
suthagar23 <suthagar.14@cse.mrt.ac.lk>
2019-05-18 23:24:22 +0530

    Added OpenMRS Colors

---
+++ OpenMRS-iOS-Client/Views/LaunchScreenController.swift
@@ -0,0 +1,14 @@
+//
+//  SplashScreen.swift
+//  OpenMRS-iOS-Client
+//
+//  Created by Kailayapathy Suthagar on 5/18/19.
+//  Copyright © 2019 OpenMRS. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SplashScreenController: UIViewController {
+    
+}
